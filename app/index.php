1ef80a2d73fdaa2fb584c7849926c88a696b6bcb
deniro21 <mr.denister@gmail.com>
2016-12-05 12:24:23 +0300

    с

---
+++ app/index.php
@@ -0,0 +1,1 @@
+<div style="font-size: 70px;">Hello! I am app :)</div>
---
+++ index.php
@@ -0,0 +1,4 @@
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.js"></script>
+
+
